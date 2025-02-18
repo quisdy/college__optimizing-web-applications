9cf1202538acd9efd82cc6a4b785dea4424dffa9
quisdy <hisou.wd@gmail.com>
2025-02-18 20:49:02 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function loadHTML(elementId, filePath) {
+	fetch(filePath)
+		.then(response => response.text())
+		.then(data => {
+			document.getElementById(elementId).innerHTML = data;
+		})
+		.catch(error => console.error('Ошибка загрузки файла:', error));
+}
+
+// Загружаем header и footer с абсолютными путями
+loadHTML('header', './header.html');
+loadHTML('footer', 'footer.html');
+
+
+
+function header_burger{
+
+}
